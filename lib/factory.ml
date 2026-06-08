e2b097c0e3d4da8d1218719a90249b8a52156127
saucedeveloper <power16gd@gmail.com>
2026-06-08 10:59:06 +0200

    Add back the custom comparison function and explain why in a comment

--- lib/factory.ml
+++ lib/factory.ml
@@ -8,10 +8,111 @@ type term =
     (* meta variable, used in rule definition. Cannot appear in the tableau *)
   | App of name * term list
   | Bind of name * term
-  
-let term_compare (a: term) (b: term) = compare a b
 
-let term_equal a b = (a = b)
+(* Address / unique id for x for printing *)
+let address_of x = 2 * (Obj.magic x) / 2
+
+let string_address_of x =
+  let s = string_of_int (address_of x) in
+  let i = 4 in
+  String.sub s (String.length s - i) i
+
+let name_compare a b = compare a b
+(* Name comparison: by id, then by length, then by contents *)
+  (* if a == b then
+    0
+  else
+    let length_comparison = compare (String.length a) (String.length b) in
+    if length_comparison <> 0 then
+      length_comparison
+    else
+      compare a b *)
+
+(*
+We use a set of custom compare functions instead
+of the builtin polymorphic compare function as its
+behaviour is not well understood. We know that it
+does not always perform comparison by memory address
+as 2 different instances of Mvar ("p") compared equal
+by the polymorphic compare function. It also makes
+unit tests possible at all because we know what to
+expect. 
+*)
+
+let term_bvar_compare a_index b_index = compare a_index b_index
+
+let term_fvar_compare a_name b_name = name_compare a_name b_name
+
+let term_mvar_compare a_name b_name = name_compare a_name b_name
+
+(* Compare App variants for Set implementation *)
+let rec term_app_compare a_name a_terms b_name b_terms =
+  let name_comparison = name_compare a_name b_name in
+  if name_comparison <> 0 then
+    name_comparison
+  else
+    let arity_comparison = compare (List.length a_terms) (List.length b_terms) in
+    if arity_comparison <> 0 then
+      arity_comparison
+    else
+      let () = assert ((List.length a_terms) = (List.length b_terms)) in
+      let combined_terms = List.combine a_terms b_terms in
+      let term_tuple_compare tup = match tup with (a, b) -> term_compare a b in
+      let term_comparisons = List.map term_tuple_compare combined_terms in
+      let non_zero_comparison c = c <> 0 in
+      let first_non_zero_comparison = List.find_opt non_zero_comparison term_comparisons in
+      Option.value first_non_zero_comparison ~default:0
+
+(* Compare Bind variants for Set implementation *)
+and term_bind_compare a_name a_term b_name b_term =
+  let name_comparison = name_compare a_name b_name in
+      if name_comparison <> 0 then
+        name_comparison
+      else
+        term_compare a_term b_term
+
+(* Compare terms for Set implementation (based on memory id) *)
+and term_compare (a: term)(b: term) =
+  if a == b then
+    0
+  else
+    term_struct_compare a b
+
+(* Compare terms by variant index then by contents recursively *)
+and term_struct_compare a b =
+  match (a, b) with
+  | (Bvar a_bvar, Bvar b_bvar) -> term_bvar_compare a_bvar b_bvar
+  | (Bvar _, _) -> -1
+  | (_, Bvar _) -> 1
+  | (Fvar a_fvar, Fvar b_fvar) -> term_fvar_compare a_fvar b_fvar
+  | (Fvar _, _) -> -1
+  | (_, Fvar _) -> 1
+  | (Mvar a_mvar, Mvar b_mvar) -> term_mvar_compare a_mvar b_mvar
+  | (Mvar _, _) -> -1
+  | (_, Mvar _) -> 1
+  | (App (a_app_name, a_app_terms), App (b_app_name, b_app_terms)) ->
+      term_app_compare a_app_name a_app_terms b_app_name b_app_terms
+  | (App _, _) -> -1
+  | (_, App _) -> 1
+  | (Bind (a_bind_name, a_bind_term), Bind (b_bind_name, b_bind_term)) ->
+      term_bind_compare a_bind_name a_bind_term b_bind_name b_bind_term
+
+(* let term_compare (a: term) (b: term) =
+  let std_comparison = compare a b in
+  let id_a = address_of a in
+  let id_b = address_of b in
+  let address_comparison = compare id_a id_b in
+  if std_comparison <> address_comparison then
+    let () = Printf.printf
+      "[term_compare] a: %s, b: %s, id_a: %d, id_b: %d, std_comparison: %d, address_comparison: %d\n"
+      (string_of_term a) (string_of_term b)
+      id_a id_b std_comparison address_comparison in
+    let () = assert (std_comparison = address_comparison) in
+    std_comparison
+  else
+    std_comparison *)
+
+let term_equal a b = (a == b)
 
 (* Module for Set implementation *)
 module FactoryTerm = struct
@@ -44,14 +145,6 @@ let string_of_term term =
       | Bind (b, body) -> Printf.sprintf "%s.(%s)" b (string_of_term body)
     in
     string_of_term term
-
-(* Address / unique id for x for printing *)
-let address_of x = 2 * (Obj.magic x) / 2
-
-let string_address_of x =
-  let s = string_of_int (address_of x) in
-  let i = 4 in
-  String.sub s (String.length s - i) i
 
 (* Create or get Bvar in factory *)
 let create_bvar index factory =
@@ -193,11 +286,11 @@ let main () =
   *)
 
   let factory1 = empty in
-  let (e, factory2) = create_mvar "e" factory1 in
-  let (p, factory3) = create_mvar "p" factory2 in
+  let (p, factory2) = create_mvar "p" factory1 in
+  let (e, factory3) = create_mvar "e" factory2 in
   let (_, factory4) = create_mvar "p" factory3 in
-  let (arg_a, factory5) = create_bind "exists" p factory4 in
-  let (arg_b, factory6) = create_bind "forall" e factory5 in
+  let (arg_b, factory5) = create_bind "forall" e factory4 in
+  let (arg_a, factory6) = create_bind "exists" p factory5 in
   Printf.printf "arg_b: %s\n" (string_address_of arg_b);
 
   let (arg_b2, factory7) = create_bind "forall" e factory6 in
