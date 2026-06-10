5e82c8129a3b25410032e69f5fde1301409f6697
saucedeveloper <power16gd@gmail.com>
2026-06-10 13:59:12 +0200

    Add term symbol type, pstring node, examples, overall changing the implementation to contain symbols in the pstrings themselves

--- lib/pstring.ml
+++ lib/pstring.ml
@@ -1,12 +1,35 @@
 open Factory
 
-(*
-Path string: list of indices decribing the traversal of a term
-| Bvar | Fvar | Mvar -> leaf
-| App -> index in {0..arity}
-| Bind -> 0 (* Because there is only one term *)
-*)
-type t = int list
+type term_symbol_variant =
+| SymBvar
+| SymFvar
+| SymMvar
+| SymApp
+| SymBind
+
+(* Identifies the term symbol uniquely *)
+type term_symbol = {
+  variant: term_symbol_variant;
+  name: Factory.name;
+}
+
+type pstring_node = {
+  symbol: term_symbol;
+  index: int;
+}
+
+(* Path string: array of index/symbol pairs decribing the traversal of a term *)
+type t = pstring_node array
+
+let pstring_node_root_index = -1
+
+let get_term_symbol term =
+  match term with
+  | Bvar index -> { variant = SymBvar; name = string_of_int index }
+  | Fvar name -> { variant = SymFvar; name = name }
+  | Mvar name -> { variant = SymMvar; name = name }
+  | App (name, _) -> { variant = SymApp; name = name }
+  | Bind (name, _) -> { variant = SymBind; name = name }
 
 let list_map_index (f: 'a -> int -> 'b) (list: 'a list) =
   let rec recursive remainder index = match remainder with
@@ -16,10 +39,83 @@ let list_map_index (f: 'a -> int -> 'b) (list: 'a list) =
   recursive list 0
 
 (* Make all the pstrings / root-to-leaf traversals in `term` *)
+(*
+t = f('x, ~exists.(P(?z)), 'y, P(?z))
+
+t = f(            (* ^.f *)
+                  [(-1, f)]
+  [0] -> 'x,      (* ^.f.0.'x *) ->
+                  [(-1, f); (0, 'x)]
+  [1] -> ~exists.( (* ^.f.1.~exists *)
+                  [(-1, f); (1, ~exists)]
+    [0] -> P(     (* ^.f.1.~exists.0.P *)
+                  [(-1, f); (1, ~exists); (0, P)]
+      [0] -> ?z   (* ^.f.1.~exists.0.P.0.?z *) ->
+                  [(-1, f); (1, ~exists); (0, P); (0, ?z)]
+    )
+  ),
+  [2] -> 'y,      (* ^.f.2.'y *) ->
+                  [(-1, f); (2, 'y)]
+  [3] -> P(       (* ^.f.3.P *)
+                  [(-1, f); (3, P)]
+    [0] -> ?z     (* ^.f.3.P.0.?z *) ->
+                  [(-1, f); (3, P); (0, ?z)]
+  )
+)
+*)
+
 let make_pstrings term =
-  (* Returns the list of created paths *)
-  let rec recursive (term: Factory.term) (current_path: t) (paths: t list) = (
+  let rec recursive (term: Factory.term) (current_path: t) (current_index: int) =
+    let created_node = { index = current_index; symbol = get_term_symbol term } in
+    let resulting_path = Array.append current_path [|created_node|] in
     match term with
+    | Bvar _ | Fvar _ | Mvar _ -> [resulting_path]
+    | App (name, terms) -> (
+      let f inner index = recursive inner resulting_path index in
+      let created_paths_by_term = list_map_index f terms in
+      let inner_created = List.concat created_paths_by_term in
+      inner_created
+    )
+    | Bind (name, inner) -> (
+      let inner_created = recursive inner resulting_path 0 in
+      inner_created
+    )
+  in
+  recursive term [||] pstring_node_root_index
+
+  (* (* Returns the list of created paths *)
+  let rec recursive (path_total: t) (path_last: pstring_node option) (term: Factory.term) = (
+    match path_total with
+    | [] -> (
+      let root_node = { index = pstring_node_root_index; symbol = get_term_symbol term } in
+      let new_path = [root_node] in
+      let created = recursive new_path (Some root_node) term in
+      created
+    )
+    | _ -> (
+      match path_last with
+      | None -> assert(false);
+      | Some last -> (
+        let created = { index = last.index; symbol = get_term_symbol term } in
+        match term with
+        | Bvar _ | Fvar _ | Mvar _ -> created
+        | App (name, terms) -> (
+          let new_path_total = path_total @ [created] in
+          let new_path_last = created in
+          let created_recursively = List.map (recursive new_path_total (Some new_path_last)) terms in
+          let created = [created] @ (List.concat created_recursively) in
+          created
+        )
+        | Bind (name, term) -> (
+          let new_path_total = path_total::created in
+          let new_path_last = created in
+          let created_recursively = recursive new_path_total (Some new_path_last) term in
+          let created = created @ [created_recursively] in
+          created
+        )
+      )
+    )
+    (* match term with
     | Bvar _ | Fvar _ | Mvar _ -> [current_path]
     | App (name, terms) -> (
       let get_inner_paths inner index =
@@ -32,9 +128,9 @@ let make_pstrings term =
     | Bind (name, inner) -> (
       let updated_path = current_path @ [0] in
       recursive inner updated_path paths
-    )
+    ) *)
   ) in
-  recursive term [] []
+  recursive [] None term *)
 
 let get_subterm term index =
   match term with
@@ -49,8 +145,8 @@ let get_subterm term index =
 let string_of_pstring_in term pstring =
   let rec recursive (pstring_rem: t)(current_term: term) =
     match pstring_rem with
-    | [] -> ""
-    | [last_index] -> (
+    | [||] -> ""
+    | [|last_index|] -> (
       let subterm = get_subterm current_term last_index in
       match subterm with
       | Some subterm -> (
@@ -61,7 +157,9 @@ let string_of_pstring_in term pstring =
       )
       | None -> assert(false);
     )
-    | index::new_pstring_rem ->
+    | _ ->
+      let index = Array.get pstring_rem 0 in
+      let new_pstring_rem = Array.slice pstring_rem 1 ((Array.length pstring_rem) - 1) in
       let subterm = get_subterm current_term index in
       match subterm with
       | None -> assert(false);
@@ -75,6 +173,47 @@ let string_of_pstring_in term pstring =
   (Factory.identifier_of_term term) ^ recursive pstring term
 
 let string_of_pstring pstr = String.concat "." (List.map string_of_int pstr)
+
+(*
+Path index for
+t = f('x, exists.(P(?z)), 'y, P(?z))
+
+               (0)
+                | f
+               (1)
+     ///////////-\\\\\\\
+  0 /       1 /   \ 2   \ 3
+  (2)       (3)   (4)   (5)
+'x |  exists |  'y |   P |
+  (6)       (7)   (8)   (11)
+  {t}      0 |    {t}  0 |
+            (10)        (13)
+           P |        ?z |
+            (12)        (15)
+           0 |          {t}
+            (14)
+          ?z |
+            (16)
+            {t}
+*)
+
+type index_node = {
+  term_id: Factory.name;
+  sub_nodes: index_node array
+}
+
+(* type term_id_variant =
+| BvarId
+| FvarId
+| MvarId
+| AppId
+| BindId
+
+(* Term identifier used in the index *)
+type term_id = {
+  variant: term_id_variant;
+  id: Factory.name;
+} *)
 
 let _ =
   let factory0 = Factory.empty in
