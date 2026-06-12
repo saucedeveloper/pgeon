86234728f6bc9a6ba5ae0e41c1dc65f7f635dcc0
saucedeveloper <power16gd@gmail.com>
2026-06-12 15:18:21 +0200

    Implement pstring removal in the index and test it by printing it out

--- lib/pstring.ml
+++ lib/pstring.ml
@@ -225,6 +225,10 @@ let term_set_add (term_set: term_set) (term: Factory.term) =
   IndexLeafTermSet.replace term_set term ();
   ()
 
+let term_set_remove (term_set: term_set) (term: Factory.term) =
+  IndexLeafTermSet.remove term_set term;
+  ()
+
 let term_set_singleton ?(capacity=8) (term: Factory.term) =
   let created_term_set = IndexLeafTermSet.create capacity in
   IndexLeafTermSet.add created_term_set term ();
@@ -266,11 +270,19 @@ let index_array_node_replace (container: index_array_node) (index: int) (map_nod
   Hashtbl.replace container index map_node;
   ()
 
+let index_array_node_remove (container: index_array_node) (index: int) =
+  Hashtbl.remove container index;
+  ()
+
 let index_array_node_make (index_map_nodes: index_map_node list) =
   let pair_with_index i node = (i, node) in
   Hashtbl.of_seq (List.to_seq (List.mapi pair_with_index index_map_nodes))
 
 let index_map_node_create ?(capacity=8) () = Hashtbl.create capacity
+
+let index_map_node_remove (container: index_map_node) (symbol: term_symbol) =
+  Hashtbl.remove container symbol;
+  ()
 
 let rec string_repeat str count =
   match count with
@@ -328,6 +340,9 @@ let debug_string_of_option (string_of: 'a -> string) (x: 'a option) =
   | Some value -> "Some(" ^ (string_of value) ^ ")"
   | None -> "None"
 
+let term_index_create ?(capacity=8) () =
+  { root = Hashtbl.create capacity }
+
 let term_index_insert (term_index: term_index) (pstring: t) (term: Factory.term) =
   let rec insert_map (node: index_map_node) (pstring_i: int) =
     (* Printf.printf "> insert_map %d\n" pstring_i; *)
@@ -382,7 +397,7 @@ let term_index_insert (term_index: term_index) (pstring: t) (term: Factory.term)
       If target does not exist -> insert at target_i an empty map and insert_map
     *)
     let target_search = Hashtbl.find_opt node target_i in
-    let target_exists = match target_search with Some _ -> true | None -> false in
+    (* let target_exists = match target_search with Some _ -> true | None -> false in *)
     (* Printf.printf ">> target_exists %s\n" (if target_exists then "true" else "false"); *)
     match target_search with
     | Some target -> insert_map target pstring_i
@@ -394,8 +409,68 @@ let term_index_insert (term_index: term_index) (pstring: t) (term: Factory.term)
   in
   insert_map term_index.root 0
 
-let term_index_create ?(capacity=8) () =
-  { root = Hashtbl.create capacity }
+let term_index_remove (term_index: term_index) (pstring: t) (term: Factory.term) =
+  let rec remove_map (node: index_map_node) (pstring_i: int) =
+    assert (0 <= pstring_i && pstring_i < (Array.length pstring));
+    let target_symbol: term_symbol = (Array.get pstring pstring_i).symbol in
+    let found_subnode = Hashtbl.find_opt node target_symbol in
+    let pstring_at_end = (pstring_i = (Array.length pstring) - 1) in
+    match found_subnode with
+    | None -> (
+      (* Nothing to delete *)
+      assert (false);
+    )
+    | Some subnode -> (
+      (* If array, assert pstring not at end, remove_array of the subarray *)
+      (* If leaf, assert pstring at end, remove from set, possibly deleting the leaf if becomes empty *)
+      match subnode with
+      | SubArray subarray -> (
+        assert (not pstring_at_end);
+        remove_array subarray (pstring_i + 1);
+        if (Hashtbl.length subarray) = 0 then
+          let _ = Hashtbl.remove node target_symbol in
+          ()
+        else
+          ()
+      )
+      | SubLeaf term_set -> (
+        assert (pstring_at_end);
+        assert (IndexLeafTermSet.mem term_set term); (* term is present *)
+        term_set_remove term_set term;
+        assert (not (IndexLeafTermSet.mem term_set term)); (* term is removed *)
+        if (IndexLeafTermSet.length term_set) = 0 then
+          let length_before = Hashtbl.length node in
+          Hashtbl.remove node target_symbol;
+          let length_after = Hashtbl.length node in
+          assert (length_after = length_before - 1); (* empty set is removed *)
+          ()
+        else
+          ()
+      )
+    )
+  (* Hashtbl.length node *)
+  and remove_array (node: index_array_node) (pstring_i: int) =
+    assert (0 <= pstring_i && pstring_i < (Array.length pstring));
+    let target_i: int = (Array.get pstring pstring_i).index in
+    let target_search = Hashtbl.find_opt node target_i in
+    (*
+    If submap exists remove_map and if |map| = 0 remove its entry
+    *)
+    match target_search with
+    | Some subnode -> (
+      remove_map subnode pstring_i;
+      if (Hashtbl.length subnode) = 0 then
+        let _ = index_array_node_remove node target_i in
+        ()
+      else
+        ()
+    )
+    | None -> (
+      let _ = assert (false) in (* Nothing to remove *)
+      ()
+    )
+  in
+  remove_map term_index.root 0
 
 let _ =
   let make_hashtbl (list: ('a * 'b) list) =
@@ -476,9 +551,8 @@ let _ =
   } in
   Printf.printf "manual_index: %s\n\n" (string_of_term_index manual_index);
 
-  let f1_pstrings = make_pstrings f1 in
-  Printf.printf "f1_pstrings: %s\n" (String.concat ", " (List.map string_of_pstring f1_pstrings));
   let procedural_index = term_index_create () in
+
   let print_insertions arg =
     let (pstring, term) = arg in
     Printf.printf "pstring: %s\n" (string_of_pstring pstring);
@@ -486,13 +560,32 @@ let _ =
     Printf.printf "procedural_index: %s\n" (string_of_term_index procedural_index);
     ()
   in
+
   let print_insertions_many fterm =
-    Printf.printf "term: %s\n" (Factory.string_of_term fterm);
+    Printf.printf "\nterm: %s (%s)\n" (Factory.string_of_term fterm) (Factory.string_address_of fterm);
     let pstrings = make_pstrings fterm in
     let pstrings_with_term = List.map (fun pstr -> (pstr, fterm)) pstrings in
     List.iter print_insertions pstrings_with_term;
     Printf.printf "\n";
   in
 
+  let print_deletions arg =
+    let (pstring, term) = arg in
+    Printf.printf "pstring: %s\n" (string_of_pstring pstring);
+    let () = term_index_remove procedural_index pstring term in
+    Printf.printf "procedural_index: %s\n" (string_of_term_index procedural_index);
+    ()
+  in
+
+  let print_deletions_many fterm =
+    Printf.printf "\nterm: %s (%s)\n" (Factory.string_of_term fterm) (Factory.string_address_of fterm);
+    let pstrings = make_pstrings fterm in
+    let pstrings_with_term = List.map (fun pstr -> (pstr, fterm)) pstrings in
+    List.iter print_deletions pstrings_with_term;
+    Printf.printf "\n";
+  in
+
   List.iter print_insertions_many terms;
+  Printf.printf "\n\nDeletions:\n\n";
+  List.iter print_deletions_many terms;
   ;;
