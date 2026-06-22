c1c4325d7093b81e5433a4a29c25280ca8e464ea
saucedeveloper <power16gd@gmail.com>
2026-06-22 16:16:03 +0200

    Add an assert, string_of_state, and state_of_int

--- lib/lfsr_random.ml
+++ lib/lfsr_random.ml
@@ -11,6 +11,7 @@ let next n0 =
   n3
 
 let get_current state bound_max =
+  assert (0 < bound_max);
   abs (state mod bound_max)
 
 let get_current_raw state = state
@@ -19,3 +20,7 @@ let get_current_raw_positive state = abs state
 
 let get_next state bound_max =
   (get_current state bound_max, next state)
+
+let string_of_state = string_of_int
+
+let state_of_int i = i
--- lib/lfsr_random.mli
+++ lib/lfsr_random.mli
@@ -11,3 +11,7 @@ val get_current_raw : t -> int
 val get_current_raw_positive : t -> int
 
 val get_next : t -> (int * t)
+
+val string_of_state : t -> string
+
+val state_of_int : int -> t
