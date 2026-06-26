14e8623e64f8ab1533b7c53a2a11dd3b52542390
saucedeveloper <power16gd@gmail.com>
2026-06-26 09:15:20 +0200

    Correct identifiers used in interface file

--- lib/pstring.mli
+++ lib/pstring.mli
@@ -1,15 +1,15 @@
 (* Node of a path string: identifies an index then a symbol *)
-type pstring_node = {
+type node = {
   index : int;
   symbol : Term_symbol.t;
 }
 
 (* Path string: array of index/symbol pairs decribing the traversal of a term *)
-type t = pstring_node array
+type t = node array
 
 val node_root_index : int
 
-val all_of_term : (Factory.term -> t list)
+val all_of_term : (Term.t -> t list)
 
 val string_of_node : (node -> string)
 
