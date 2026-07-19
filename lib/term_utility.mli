77c3fc142a1f0f57f889f676f2ebf6264cc86601
saucedeveloper <power16gd@gmail.com>
2026-07-19 18:41:40 +0200

    Add an attempt at a term parser for easier generation

--- lib/term_utility.mli
+++ lib/term_utility.mli
@@ -6,6 +6,13 @@ type template =
 | TApp  of Term.name * template list
 | TBind of Term.name * template
 
+type term_id =
+| IBvar of int
+| IFvar of Term.name
+| IMvar of Term.name
+| IApp  of Term.name
+| IBind of Term.name
+
 val tbvar : int -> template
 val tfvar : string -> template
 val tmvar : string -> template
@@ -18,3 +25,9 @@ val create_from_template : template -> Term.factory -> Term.t * Term.factory
 val create_many : template Seq.t -> Term.factory -> Term.t list * Term.factory
 
 val string_of_full : ?n:int -> Term.t -> string
+
+val extract_identifier : string -> (term_id * int) option
+
+val string_of_term_id : term_id -> string
+
+val parse : string -> Term.factory -> (Term.t * Term.factory) option
