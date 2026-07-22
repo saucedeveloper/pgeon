c3fe3365481342b435f332d02fed893310690f1c
saucedeveloper <power16gd@gmail.com>
2026-07-22 15:51:31 +0200

    Replace the use of find_pstring with contains_term and add test with contains_all_terms

--- lib/test/term_index_test_maintenance.ml
+++ lib/test/term_index_test_maintenance.ml
@@ -25,56 +25,15 @@ let (terms, _factory1) = Term_utility.create_many (List.to_seq templates) Term.e
 let index_with_terms = Term_index.add_terms Term_index.empty (List.to_seq terms)
 let index_with_terms_removed = Term_index.remove_terms index_with_terms (List.to_seq terms)
 
-let expected_pstring0 = Pstring.( Term_symbol.[|
-  { index = node_root_index; symbol = SymBind "W" };
-  { index = 0; symbol = SymApp "g" };
-  { index = 0; symbol = SymMvar };
-|] )
-
-let expected_pstring1 = Pstring.( Term_symbol.[|
-  { index = node_root_index; symbol = SymBind "W" };
-  { index = 0; symbol = SymApp "g" };
-  { index = 1; symbol = SymApp "f" };
-  { index = 0; symbol = SymBvar 0 };
-|] )
-
-let expected_pstring2 = Pstring.( Term_symbol.[|
-  { index = node_root_index; symbol = SymBind "W" };
-  { index = 0; symbol = SymApp "g" };
-  { index = 1; symbol = SymApp "f" };
-  { index = 1; symbol = SymApp "a" };
-|] )
-
-let expected_pstring3 = Pstring.( Term_symbol.[|
-  { index = node_root_index; symbol = SymBind "W" };
-  { index = 0; symbol = SymApp "g" };
-  { index = 1; symbol = SymApp "f" };
-  { index = 2; symbol = SymFvar };
-|] )
-
-(* index contains W.0.g.0.?? *)
-let%test _ = Option.is_some (Term_index.find_pstring index_with_terms expected_pstring0)
-
-(* index contains W.0.g.1.f.0.#0 *)
-let%test _ = Option.is_some (Term_index.find_pstring index_with_terms expected_pstring1)
-
-(* index contains W.0.g.1.f.1.a *)
-let%test _ = Option.is_some (Term_index.find_pstring index_with_terms expected_pstring2)
-
-(* index contains W.0.g.1.f.1.'' *)
-let%test _ = Option.is_some (Term_index.find_pstring index_with_terms expected_pstring3)
-
-(* index contains W.0.g.0.?? *)
-let%test _ = Option.is_none (Term_index.find_pstring index_with_terms_removed expected_pstring0)
-
-(* index contains W.0.g.1.f.0.#0 *)
-let%test _ = Option.is_none (Term_index.find_pstring index_with_terms_removed expected_pstring1)
-
-(* index contains W.0.g.1.f.1.a *)
-let%test _ = Option.is_none (Term_index.find_pstring index_with_terms_removed expected_pstring2)
-
-(* index contains W.0.g.1.f.1.'' *)
-let%test _ = Option.is_none (Term_index.find_pstring index_with_terms_removed expected_pstring3)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 0)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 1)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 2)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 3)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 4)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 5)
+let%test _ = Term_index.contains_term index_with_terms (List.nth terms 6)
+let%test _ = assert ((List.length templates) = 7); true
+let%test _ = Term_index.contains_all_terms index_with_terms (List.to_seq terms)
 
 (* index is empty *)
 let%test _ = Term_index.is_empty index_with_terms_removed
