67a52e72f998865c14898459a41c01c13215a7eb
saucedeveloper <power16gd@gmail.com>
2026-07-22 16:27:35 +0200

    Add cases for the negation of contains_term

--- lib/test/term_index_test_maintenance.ml
+++ lib/test/term_index_test_maintenance.ml
@@ -10,6 +10,9 @@ let t_appg = Term_utility.tapp "g" [t_mvar; t_appf]
 (* W.g(?Y, f(#0, a, 'x)) *)
 let t_bind = Term_utility.tbind "W" t_appg
 
+let t_not_indexed_0 = Term_utility.tbind "W" t_appf
+let t_not_indexed_1 = Term_utility.tbind "W" t_const
+
 let templates = [
   t_bind;
   t_bvar;
@@ -18,12 +21,15 @@ let templates = [
   t_const;
   t_appf;
   t_appg;
+  t_not_indexed_0;
+  t_not_indexed_1;
 ]
 
 let (terms, _factory1) = Term_utility.create_many (List.to_seq templates) Term.empty_factory
 
-let index_with_terms = Term_index.add_terms Term_index.empty (List.to_seq terms)
-let index_with_terms_removed = Term_index.remove_terms index_with_terms (List.to_seq terms)
+let indexed_terms = Seq.take 7 (List.to_seq terms)
+let index_with_terms = Term_index.add_terms Term_index.empty indexed_terms
+let index_with_terms_removed = Term_index.remove_terms index_with_terms indexed_terms
 
 let%test _ = Term_index.contains_term index_with_terms (List.nth terms 0)
 let%test _ = Term_index.contains_term index_with_terms (List.nth terms 1)
@@ -32,8 +38,20 @@ let%test _ = Term_index.contains_term index_with_terms (List.nth terms 3)
 let%test _ = Term_index.contains_term index_with_terms (List.nth terms 4)
 let%test _ = Term_index.contains_term index_with_terms (List.nth terms 5)
 let%test _ = Term_index.contains_term index_with_terms (List.nth terms 6)
-let%test _ = assert ((List.length templates) = 7); true
-let%test _ = Term_index.contains_all_terms index_with_terms (List.to_seq terms)
+let%test _ = not (Term_index.contains_term index_with_terms (List.nth terms 7))
+let%test _ = not (Term_index.contains_term index_with_terms (List.nth terms 8))
+let () = assert ((List.length templates) = 9)
+let%test _ = Term_index.contains_all_terms index_with_terms indexed_terms
 
 (* index is empty *)
 let%test _ = Term_index.is_empty index_with_terms_removed
+
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 0))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 1))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 2))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 3))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 4))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 5))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 6))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 7))
+let%test _ = not (Term_index.contains_term index_with_terms_removed (List.nth terms 8))
